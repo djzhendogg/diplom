9a718c76d13b003a5b6486efd86a0cbdfb9d17ff
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-03-13 20:38:54 +0300

    go gpu

--- experiment/fgw_gpu.py
+++ experiment/fgw_gpu.py
@@ -1,38 +1,30 @@
 import json
 import os
+
 import pandas as pd
-import time
 
 from fgw import FusedGromovWassersteinComputer
 from preset_tools_difflen import encode_seqs
+
 path = "./data"
-file = "amp_gonzales.csv"
+save_path = "./ot_result/fgw_par/"
+files = ['antimicrobial_UniDL4BioPep.csv', 'antibacterial.csv']
 
-df = pd.read_csv(os.path.join(path, file))
-df_0 = df[df['label'] == 0]
-df_1 = df[df['label'] == 1]
+for file in files:
+    df = pd.read_csv(os.path.join(path, file))
+    df_0 = df[df['label'] == 0]
+    df_1 = df[df['label'] == 1]
 
-X0 = encode_seqs(df_0['sequence'].to_list())
-X1 = encode_seqs(df_1['sequence'].to_list())
+    X0 = encode_seqs(df_0['sequence'].to_list())
+    X1 = encode_seqs(df_1['sequence'].to_list())
 
-# Создаем экземпляр компьютера для каждого процесса
-print("\n" + "=" * 60)
-print("ОБРАБОТКА GPU")
-fgw_computer = FusedGromovWassersteinComputer()
-dist_res = fgw_computer.compute_fgw_with_search_gpu(X0, X1)
-dist_res['len_0'] = len(X0)
-dist_res['len_1'] = len(X1)
-print(dist_res)
-print("\n" + "=" * 60)
-print()
+    fgw_computer = FusedGromovWassersteinComputer()
+    dist_res = fgw_computer.compute_fgw_with_search_gpu(X0, X1)
+    dist_res['len_0'] = len(X0)
+    dist_res['len_1'] = len(X1)
 
-print("\n" + "=" * 60)
-print("ОБРАБОТКА без GPU")
+    name = file.split('.')[0]
+    output_path = os.path.join(save_path, f'{name}.json')
 
-fgw_computer = FusedGromovWassersteinComputer()
-dist_res = fgw_computer.compute_fgw_with_search(X0, X1)
-dist_res['len_0'] = len(X0)
-dist_res['len_1'] = len(X1)
-print(dist_res)
-print("\n" + "=" * 60)
-
+    with open(output_path, 'w', encoding='utf-8') as f:
+        json.dump(dist_res, f, ensure_ascii=False, indent=4)
