d3ac2183c29c88c9296cbd861731c329219058ac
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-03-21 20:37:44 +0300

    umami

--- experiment/plm_embeddings/prott5.py
+++ experiment/plm_embeddings/prott5.py
@@ -50,8 +50,9 @@ def prott5_encoding(sequences, model_name=None, batch_size=16):
 def main():
     embeddings_type = 'prott5'
     data_path = '../data'
-    files = [f.split('.')[0] for f in os.listdir(data_path)
-             if os.path.isfile(os.path.join(data_path, f)) and f.endswith('.csv')]
+    files = ['umami']
+    # files = [f.split('.')[0] for f in os.listdir(data_path)
+    #          if os.path.isfile(os.path.join(data_path, f)) and f.endswith('.csv')]
     process_data_files(files, data_path, embeddings_type, prott5_encoding)
 
 
