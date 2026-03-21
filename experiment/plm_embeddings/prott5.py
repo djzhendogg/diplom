22ac0ec34118de23b3320a9a27282f44c4f14b55
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-03-21 18:57:44 +0300

    check embeddings8

--- experiment/plm_embeddings/prott5.py
+++ experiment/plm_embeddings/prott5.py
@@ -49,9 +49,7 @@ def prott5_encoding(sequences, model_name=None, batch_size=16):
 
 def main():
     embeddings_type = 'prott5'
-
-    data_path = '../data'
-    files = [os.path.join(data_path, f) for f in ['umami.csv']]
+    files = ['umami.csv']
     # files = [os.path.join(data_path, f) for f in os.listdir(data_path)
     #          if os.path.isfile(os.path.join(data_path, f)) and f.endswith('.csv')]
     process_data_files(files, embeddings_type, prott5_encoding)
