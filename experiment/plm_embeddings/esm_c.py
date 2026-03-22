7056199a0f61c4c44e9f9fe37bcd873a2f5a5d0b
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-03-22 19:27:43 +0300

    esmc try4

--- experiment/plm_embeddings/esm_c.py
+++ experiment/plm_embeddings/esm_c.py
@@ -1,5 +1,3 @@
-import os
-
 import numpy as np
 import torch
 from esm.models.esmc import ESMC
@@ -19,15 +17,9 @@ def esm_c_encoding(sequences, model_name="esmc_600m"):
     Returns:
         numpy.ndarray: Array of sequence embeddings (n_sequences, embedding_dim)
     """
-    # device = setup_torch_device()
-    model = ESMC.from_pretrained(model_name).to('cpu')
+    device = setup_torch_device()
+    model = ESMC.from_pretrained(model_name).to(device)
     model.eval()
-
-    config_mean = LogitsConfig(
-        sequence=True,
-        return_embeddings=True,
-        return_mean_embedding=True,
-    )
 
     config = LogitsConfig(
         sequence=True,
@@ -45,34 +37,23 @@ def esm_c_encoding(sequences, model_name="esmc_600m"):
                 protein_tensor,
                 config
             )
-            sequence_embeddings = logits_output.embeddings.cpu().numpy()
-            np.save(f"sequence_embeddings_{sequence}.npy", sequence_embeddings)
 
-            logits_output_mean = model.logits(
-                protein_tensor,
-                config_mean
-            )
+            sequence_embeddings = logits_output.embeddings.squeeze(0)
+            pooled_embedding = sequence_embeddings.mean(dim=0).cpu().numpy()
+            embeddings.append(pooled_embedding)
 
-            sequence_embeddings_mean = logits_output_mean.mean_embedding.cpu().numpy()
-            np.save(f"sequence_embeddings_mean_{sequence}.npy", sequence_embeddings_mean)
-
-            # sequence_embeddings = logits_output.embeddings.squeeze(0)
-            # pooled_embedding = sequence_embeddings.mean(dim=0).cpu().numpy()
-            # embeddings.append(pooled_embedding)
-
-    return sequence_embeddings_mean
+    return np.vstack(embeddings)
 
 
-# def main():
-#     embeddings_type = 'esm_c'
-#
-#     data_path = '../data'
-#     files = [os.path.join(data_path, f) for f in ['umami.csv', 'amp_gonzales.csv']]
-#     # files = [os.path.join(data_path, f) for f in os.listdir(data_path)
-#     #          if os.path.isfile(os.path.join(data_path, f)) and f.endswith('.csv')]
-#     process_data_files(files, embeddings_type, esm_c_encoding)
+def main():
+    embeddings_type = 'esm_c'
+
+    data_path = '../data'
+    files = ['umami']
+    # files = [f.split('.')[0] for f in os.listdir(data_path)
+    #          if os.path.isfile(os.path.join(data_path, f)) and f.endswith('.csv')]
+    process_data_files(files, data_path, embeddings_type, esm_c_encoding)
 
 
 if __name__ == "__main__":
-    sequences = ['VVYPWTQRF', 'LDL']
-    esm_c_encoding(sequences)
+    main()
