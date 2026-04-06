3e000652d4c7ee83aacdb1075061edc361e92394
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-04-06 14:04:00 +0300

    ankh1.5

--- experiment/plm_embeddings/ankh_em.py
+++ experiment/plm_embeddings/ankh_em.py
@@ -6,23 +6,12 @@ print(dir(ankh))
 model, tokenizer = ankh.load_base_model()
 model.eval()
 
-binary_classification_model = ankh.ConvBertForBinaryClassification(
-    input_dim=768,
-    nhead=4,
-    hidden_dim=384,
-    num_hidden_layers=1,
-    num_layers=1,
-    kernel_size=7,
-    dropout=0.2,
-    pooling='max',
-)
-binary_classification_model.eval()
-
 protein_sequences = [
-    'MKALCLLLLPVLGLLVSSKTLCSMEEAINERIQEVAGSLIFRAISSIGLECQSVTSRGDLATCPRGFAVTGCTCGSACGSWDVRAETTCHCQCAGMDWTGARCCRVQPLEHHHHHH',
-    'GSHMSLFDFFKNKGSAATATDRLKLILAKERTLNLPYMEEMRKEIIAVIQKYTKSSDIHFKTLDSNQSVETIEVEIILPR',
+    'MKALCLLL',
+    'GSHMS',
 ]
-
+print(len('MKALCLLL'))
+print(len('GSHMS'))
 protein_sequences = [list(seq) for seq in protein_sequences]
 
 outputs = tokenizer(
@@ -32,12 +21,10 @@ outputs = tokenizer(
     is_split_into_words=True,
     return_tensors="pt",
 )
+print(outputs['attention_mask'])
 with torch.no_grad():
     embeddings = model(input_ids=outputs['input_ids'], attention_mask=outputs['attention_mask'])
     print(dir(embeddings))
     print(embeddings[0].shape)
 
-with torch.no_grad():
-    embeddings = binary_classification_model(input_ids=outputs['input_ids'], attention_mask=outputs['attention_mask'])
-    print(embeddings.shape)
 
