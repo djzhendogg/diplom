7c9cd27ddcce3ef67a225fac0dc68e97b226a32a
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-04-06 15:01:48 +0300

    ankh3

--- experiment/plm_embeddings/ankh_em.py
+++ experiment/plm_embeddings/ankh_em.py
@@ -1,3 +1,5 @@
+import os
+
 import ankh
 import numpy as np
 import torch
@@ -10,7 +12,7 @@ model.eval()
 model.to(device=device)
 
 
-def ankh_encoding_with_attention_mask(sequences, batch_size=32):
+def ankh_encoding(sequences, batch_size=32):
     processed_sequences = [list(seq) for seq in sequences]
     result_embeddings = []
     with torch.no_grad():
@@ -38,46 +40,22 @@ def ankh_encoding_with_attention_mask(sequences, batch_size=32):
                 mean_embeddings.append(seq_emd_mean)
 
             result_embeddings.append(mean_embeddings)
-    result_embeddings = np.vstack(result_embeddings)
-    np.save('embeddings_mask.npy', result_embeddings)
-
-
-def ankh_encoding_no_attention_mask(sequences, batch_size=32):
-    processed_sequences = [list(seq) for seq in sequences]
-    result_embeddings = []
-    with torch.no_grad():
-        for i in range(0, len(processed_sequences), batch_size):
-            batch_sequences = processed_sequences[i:i + batch_size]
-            encoded = tokenizer(
-                batch_sequences,
-                add_special_tokens=True,
-                padding=True,
-                is_split_into_words=True,
-                return_tensors="pt",
-            )
-            # print(encoded['attention_mask'])
-
-            embeddings = model(input_ids=encoded['input_ids'].to(device), attention_mask=encoded['attention_mask'].to(device))[0].cpu()
-            # print('out emb shape')
-            # print(embeddings.shape)
-
-            mean_embeddings = []
-            for seq_num in range(len(embeddings)):
-                seq_emd = embeddings[seq_num]
-                seq_emd_mean = seq_emd.mean(dim=0)
-                mean_embeddings.append(seq_emd_mean)
-
-            result_embeddings.append(mean_embeddings)
-    result_embeddings = np.vstack(result_embeddings)
-    np.save('embeddings_no_mask.npy', result_embeddings)
+    return np.vstack(result_embeddings)
 
 
 def main():
-    protein_sequences = [
-        'MKALCLLL',
-        'GSHMS',
-    ]
-    ankh_encoding_with_attention_mask(protein_sequences)
-    ankh_encoding_no_attention_mask(protein_sequences)
+    # protein_sequences = [
+    #     'MKALCLLL',
+    #     'GSHMS',
+    # ]
+    # ankh_encoding(protein_sequences)
+    embeddings_type = 'ankh_em'
+    data_path = '../data'
+    # files = [f.split('.')[0] for f in os.listdir(data_path)
+    #          if os.path.isfile(os.path.join(data_path, f)) and f.endswith('.csv')]
+    files = ['amp_gonzales']
+    process_data_files(files, data_path, embeddings_type, ankh_encoding)
+
+
 if __name__ == "__main__":
     main()
