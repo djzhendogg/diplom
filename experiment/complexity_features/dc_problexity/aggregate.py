bd987e0ed82ac1e940728e8d3a86c5d5643873ff
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-05-07 23:08:34 +0300

    clean_problexity

--- experiment/complexity_features/dc_problexity/aggregate.py
+++ experiment/complexity_features/dc_problexity/aggregate.py
@@ -9,8 +9,6 @@ save_path = 'results'
 features_config_path = 'features_config.yaml'
 
 full_features_file_name = 'problexity.csv'
-significant_features_file_name = 'problexity_significant.csv'
-uncorrelating_features_file_name = 'problexity_uncorrelating.csv'
 
 with open(features_config_path, 'r') as f:
     features = yaml.safe_load(f)
@@ -18,10 +16,5 @@ with open(features_config_path, 'r') as f:
 df = json_to_flat_df_auto(raw_path)
 df.set_index(['name'], inplace=True)
 df.sort_index(inplace=True)
+df = df[features['significant']]
 df.to_csv(os.path.join(save_path, full_features_file_name))
-
-df_uncorrelating = df[features['uncorrelating']]
-df_uncorrelating.to_csv(os.path.join(save_path, uncorrelating_features_file_name))
-
-df_significant = df[features['significant']]
-df_significant.to_csv(os.path.join(save_path, significant_features_file_name))
