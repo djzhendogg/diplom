855899cf821bb28e7b873941c90225c45d1f8870
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-05-07 23:27:53 +0300

    clean_sd

--- experiment/complexity_features/dc_sequence_diversity/aggregate.py
+++ experiment/complexity_features/dc_sequence_diversity/aggregate.py
@@ -1,27 +1,14 @@
 import os
 
-import yaml
-
 from experiment.utils.aggregations import json_to_flat_df_auto
 
 raw_path = 'results/raw'
 save_path = 'results'
-features_config_path = 'features_config.yaml'
 
-significant_features_file_name = 'sequence_diversity_significant.csv'
-uncorrelating_features_file_name = 'sequence_diversity_uncorrelating.csv'
+file_name = 'sequence_diversity.csv'
 
 df = json_to_flat_df_auto(raw_path)
 df.set_index(['name'], inplace=True)
 df.sort_index(inplace=True)
 df.drop(['samples_num'], axis=1, inplace=True)
-df.to_csv(os.path.join(save_path, significant_features_file_name))
-
-with open(features_config_path, 'r') as f:
-    features = yaml.safe_load(f)
-
-df_uncorrelating = df[features['uncorrelating']]
-df_uncorrelating.to_csv(os.path.join(save_path, uncorrelating_features_file_name))
-
-df_significant = df[features['significant']]
-df_significant.to_csv(os.path.join(save_path, significant_features_file_name))
+df.to_csv(os.path.join(save_path, file_name))
--- experiment/complexity_features/dc_sequence_diversity/select_significant.py
+++ experiment/complexity_features/dc_sequence_diversity/select_significant.py
@@ -1,20 +1,12 @@
 import pandas as pd
-import yaml
-from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import VarianceThreshold
 
-features_config_path = 'features_config.yaml'
 df = pd.read_csv('results/sequence_diversity.csv', index_col='name')
-X_scaled = StandardScaler().fit_transform(df)
-df_scaled = pd.DataFrame(X_scaled, columns=df.columns)
 sel = VarianceThreshold()
-sel.fit(df_scaled)
+sel.fit(df)
 significant_variance_features = sel.get_feature_names_out()
 to_drop = set(df.columns) - set(significant_variance_features)
 print(f"Low variance features:{to_drop}")
 
-with open(features_config_path, 'r') as f:
-    features = yaml.safe_load(f)
-features['significant'] = list(significant_variance_features)
-with open(features_config_path, 'w') as file:
-    yaml.dump(features, file)
+df_significant = df[significant_variance_features]
+df_significant.to_csv('results/sequence_diversity_significant.csv')
