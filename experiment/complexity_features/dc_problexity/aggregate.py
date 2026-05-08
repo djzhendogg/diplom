f0e99c7e150ab7cb59b046cc06ad9daffb672f83
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-05-08 19:59:58 +0300

    rename problex

--- experiment/complexity_features/dc_problexity/aggregate.py
+++ experiment/complexity_features/dc_problexity/aggregate.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import yaml
@@ -13,8 +14,13 @@ full_features_file_name = 'problexity.csv'
 with open(features_config_path, 'r') as f:
     features = yaml.safe_load(f)
 
+with open('feature_names.json', 'r', encoding='utf-8') as f:
+    feature_names = json.load(f)['names']
+
 df = json_to_flat_df_auto(raw_path)
 df.set_index(['name'], inplace=True)
 df.sort_index(inplace=True)
 df = df[features['significant']]
+df.rename(columns=feature_names, inplace=True)
+
 df.to_csv(os.path.join(save_path, full_features_file_name))
