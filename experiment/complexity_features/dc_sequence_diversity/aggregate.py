457ec77d0de602e66f4089141ea4ab230963abbf
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2026-05-08 20:04:19 +0300

    rename sd

--- experiment/complexity_features/dc_sequence_diversity/aggregate.py
+++ experiment/complexity_features/dc_sequence_diversity/aggregate.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from experiment.utils.aggregations import json_to_flat_df_auto
@@ -7,8 +8,12 @@ save_path = 'results'
 
 file_name = 'sequence_diversity.csv'
 
+with open('feature_names.json', 'r', encoding='utf-8') as f:
+    feature_names = json.load(f)['names']
+
 df = json_to_flat_df_auto(raw_path)
 df.set_index(['name'], inplace=True)
 df.sort_index(inplace=True)
 df.drop(['samples_num'], axis=1, inplace=True)
+df.rename(columns=feature_names, inplace=True)
 df.to_csv(os.path.join(save_path, file_name))
